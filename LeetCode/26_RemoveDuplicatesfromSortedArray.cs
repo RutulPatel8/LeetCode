9d653fc0a3180d7ed44920f118b6a857d99c1ab5
ADMIN <skywork2212@gmail.com>
2025-12-25 02:19:46 +0530

    Add project files.

---
+++ LeetCode/26_RemoveDuplicatesfromSortedArray.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode
+{
+    public class _26_RemoveDuplicatesfromSortedArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            Stack<int> finalArray = new Stack<int>();
+
+            foreach (var item in nums)
+            {
+                if(finalArray.Count() == 0 || finalArray.First() != item)
+                {
+                    finalArray.Push(item);
+                }
+            }
+
+
+            return finalArray.Count();
+        }
+    }
+}
