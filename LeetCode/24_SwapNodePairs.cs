6ac54c3d246e097076426ca04bccaa9df90bf285
ADMIN <skywork2212@gmail.com>
2025-12-30 02:47:29 +0530

    Swap the sortedlist

---
+++ LeetCode/24_SwapNodePairs.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode
+{
+    public class _24_SwapNodePairs
+    {
+        public static ListNode SwapPairs(ListNode head)
+        {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+
+            ListNode prev = dummy;
+
+            while (prev.next != null && prev.next.next != null)
+            {
+                ListNode first = prev.next;
+                ListNode second = first.next;
+
+                // swap
+                first.next = second.next;
+                second.next = first;
+                prev.next = second;
+
+                // move forward
+                prev = first;
+            }
+
+            return dummy.next;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LeetCode;
 
-_22_GenerateParentheses.GenerateParenthesis(3);
+ListNode head = new ListNode(1,
+                        new ListNode(2,
+                            new ListNode(3,
+                                new ListNode(4,
+                                    new ListNode(5)))));
+
+_24_SwapNodePairs.SwapPairs(head);
