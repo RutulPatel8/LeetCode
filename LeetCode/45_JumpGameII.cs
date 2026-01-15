af9ea584c433f8084153ea59bfe0e5cb1c5ee16c
ADMIN <skywork2212@gmail.com>
2026-01-15 22:45:58 +0530

    implement jump

---
+++ LeetCode/45_JumpGameII.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode
+{
+    public class _45_JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            int jump = 0;
+            int farthest = 0;
+            int currentEnd = 0;
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                farthest = Math.Max(farthest, i + nums[i]);
+
+                if (i == currentEnd)
+                {
+                    jump++;
+                    currentEnd = farthest;
+                }
+            }
+
+            return jump;
+        }
+    }
+}
