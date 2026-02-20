9f5fa879c7612c22342a19e8526df7d22d69c8dc
ADMIN <skywork2212@gmail.com>
2026-02-21 00:42:52 +0530

    Delete duplicates

---
+++ LeetCode/83_Remove_Duplicates_Sorted_List.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode
+{
+    public class _83_Remove_Duplicates_Sorted_List
+    {
+        public static ListNode DeleteDuplicates(ListNode head)
+        {
+            var current = head;
+            
+            while(current != null && current.next != null)
+            {
+                if(current != null && current.next != null)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return current;
+        }
+    }
+}
